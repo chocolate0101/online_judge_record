fe4e183eea268454867818cabca37779d0ca8977
Eric <eric900101228@gmail.com>
2026-01-23 22:34:21 +0800

    fixed some bug in leetcode 961

--- LC/0961_N_Repeated_Element_in_Size_2N_Array/solution.cpp
+++ LC/0961_N_Repeated_Element_in_Size_2N_Array/solution.cpp
@@ -1,8 +1,26 @@
+// solution 1
+class Solution {
+public:
+    int repeatedNTimes(vector<int>& nums) {
+        unordered_set<int> num_set = {};
+        for (auto& num: nums) {
+            if (num_set.count(num)) {
+                return num;
+            } else {
+                num_set.insert(num);
+            }
+        }
+
+        return nums[0];
+    }
+};
+
+// solution 2 (best solution)
 class Solution {
 public:
     int repeatedNTimes(vector<int>& nums) {
         for (int i = 0; i < nums.size() - 2; i++) {
-            if (nums[i] == nums[i+1] | nums[i] == nums[i+2]) {
+            if (nums[i] == nums[i+1] || nums[i] == nums[i+2]) {
                 return nums[i];
             }
         }
--- LC/0961_N_Repeated_Element_in_Size_2N_Array/solution.py
+++ LC/0961_N_Repeated_Element_in_Size_2N_Array/solution.py
@@ -1,11 +1,5 @@
 # solution 1
 class Solution:
-    def plusOne(self, digits: List[int]) -> List[int]:
-        integer = int("".join(str(digit) for digit in digits)) + 1
-        return list(map(int, str(integer)))
-
-# solution 2
-class Solution:
     def repeatedNTimes(self, nums: List[int]) -> int:
         d = {}
         for num in nums:
@@ -13,7 +7,7 @@ class Solution:
                 return num
             d[num] = 1
 
-# solution 3 (best solution)
+# solution 2 (best solution)
 class Solution:
     def repeatedNTimes(self, nums: List[int]) -> int:
         for i in range(len(nums) - 2):
