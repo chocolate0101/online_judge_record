51ff19f70f6ba2f4994ddebd6767967fd9eaca80
Eric <eric900101228@gmail.com>
2026-01-27 21:02:14 +0800

    add another solution of Leetcode 3314 & add cpp solution of Leetcode 3315

--- LC/3314_Construct_the_Minimum_Bitwise_Array_I/solution.cpp
+++ LC/3314_Construct_the_Minimum_Bitwise_Array_I/solution.cpp
@@ -1,3 +1,29 @@
+// solution 1
+class Solution {
+public:
+    vector<int> minBitwiseArray(vector<int>& nums) {
+        vector<int> ans;
+        bool is_find = 0;
+        for (auto& num: nums) {
+            is_find = 0;
+            for (int i = 0; i < num; i++) {
+                if ((i | (i+1)) == num) {
+                    ans.push_back(i);
+                    is_find = 1;
+                    break;
+                }
+            }
+
+            if (!is_find) {
+                ans.push_back(-1);
+            }
+        }
+
+        return ans;
+    }
+};
+
+// solution 2 (best solution)
 class Solution {
 public:
     vector<int> minBitwiseArray(vector<int>& nums) {
--- LC/3314_Construct_the_Minimum_Bitwise_Array_I/solution.py
+++ LC/3314_Construct_the_Minimum_Bitwise_Array_I/solution.py
@@ -1,3 +1,22 @@
+# solution 1
+class Solution:
+    def minBitwiseArray(self, nums: List[int]) -> List[int]:
+        ans = []
+        is_find = 0
+        for num in nums:
+            is_find = 0
+            for x in range(num):
+                if (x | (x+1)) == num:
+                    is_find = 1
+                    ans.append(x)
+                    break
+
+            if not is_find:
+                ans.append(-1)
+        
+        return ans
+
+# solution 2 (best solution)
 class Solution:
     def minBitwiseArray(self, nums: List[int]) -> List[int]:
         ans = []
