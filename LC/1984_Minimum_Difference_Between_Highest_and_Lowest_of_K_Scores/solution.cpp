2ef05d0733b80245c07a627113cb690b2433648f
Eric <eric900101228@gmail.com>
2026-02-02 20:28:08 +0800

    add another cpp & python solution of LeetCode 1984

--- LC/1984_Minimum_Difference_Between_Highest_and_Lowest_of_K_Scores/solution.cpp
+++ LC/1984_Minimum_Difference_Between_Highest_and_Lowest_of_K_Scores/solution.cpp
@@ -1,3 +1,31 @@
+// solution 1
+class Solution {
+public:
+    void backtrack(vector<int>& nums, int start_index, vector<int>& curr_comb, int& min_diff, int k, int n) {
+        if (curr_comb.size() == k) {
+            int curr_diff = *max_element(curr_comb.begin(), curr_comb.end()) - *min_element(curr_comb.begin(), curr_comb.end());
+            min_diff = min(min_diff, curr_diff);
+            return;
+        }
+
+        for (size_t i = start_index; i < n; i++) {
+            curr_comb.push_back(nums[i]);
+            backtrack(nums, i+1, curr_comb, min_diff, k, n);
+            curr_comb.pop_back();
+        }
+    }
+
+    int minimumDifference(vector<int>& nums, int k) {
+        int min_diff = INT_MAX;
+        int n = nums.size();
+        vector<int> curr_comb;
+        backtrack(nums, 0, curr_comb, min_diff, k, n);
+
+        return min_diff;
+    }
+};
+
+// solution 2
 class Solution {
 public:
     int minimumDifference(vector<int>& nums, int k) {
--- LC/1984_Minimum_Difference_Between_Highest_and_Lowest_of_K_Scores/solution.py
+++ LC/1984_Minimum_Difference_Between_Highest_and_Lowest_of_K_Scores/solution.py
@@ -1,3 +1,24 @@
+# solution 1
+class Solution:
+    def minimumDifference(self, nums: List[int], k: int) -> int:
+        self.min_diff = inf
+        n = len(nums)
+
+        def backtrack(start_index, curr_comb):
+            if len(curr_comb) == k:
+                curr_min = max(curr_comb) - min(curr_comb)
+                self.min_diff = min(self.min_diff, curr_min)
+                return
+            
+            for i in range(start_index, n):
+                curr_comb.append(nums[i])
+                backtrack(i+1, curr_comb)
+                curr_comb.pop()
+
+        backtrack(0, [])
+        return self.min_diff
+
+# solution 2
 class Solution:
     def minimumDifference(self, nums: List[int], k: int) -> int:
         if k == 1:
