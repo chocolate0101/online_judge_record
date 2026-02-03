c46f823c9287ad8169f108522430d8cc9a9688c8
Eric <eric900101228@gmail.com>
2026-02-03 20:22:35 +0800

    add another solution of Leetcode 1200

--- LC/1200_Minimum_Absolute_Difference/solution.cpp
+++ LC/1200_Minimum_Absolute_Difference/solution.cpp
@@ -1,3 +1,37 @@
+// solution 1
+class Solution {
+public:
+    vector<vector<int>> minimumAbsDifference(vector<int>& arr) {
+        int min_abs_diff = INT_MAX;
+        vector<vector<int>> min_abs_diff_list;
+
+        for (int i = 0; i < arr.size(); i++) {
+            for (int j = i+1; j < arr.size(); j++) {
+                int tmp = abs(arr[i] - arr[j]);
+                if (tmp < min_abs_diff) {
+                    min_abs_diff = tmp;
+                    min_abs_diff_list.clear();
+                    if (arr[i] < arr[j]) {
+                        min_abs_diff_list.push_back({arr[i], arr[j]});
+                    } else {
+                        min_abs_diff_list.push_back({arr[j], arr[i]});
+                    }
+                } else if (tmp == min_abs_diff) {
+                    if (arr[i] < arr[j]) {
+                        min_abs_diff_list.push_back({arr[i], arr[j]});
+                    } else {
+                        min_abs_diff_list.push_back({arr[j], arr[i]});
+                    }
+                }
+            }
+        }
+
+        sort(min_abs_diff_list.begin(), min_abs_diff_list.end());
+        return min_abs_diff_list;
+    }
+};
+
+// solution 2
 class Solution {
 public:
     vector<vector<int>> minimumAbsDifference(vector<int>& arr) {
--- LC/1200_Minimum_Absolute_Difference/solution.py
+++ LC/1200_Minimum_Absolute_Difference/solution.py
@@ -1,3 +1,29 @@
+# solution 1
+class Solution:
+    def minimumAbsDifference(self, arr: List[int]) -> List[List[int]]:
+        min_abs_diff = inf
+        min_abs_diff_list = []
+
+        for i in range(len(arr)):
+            for j in range(i+1, len(arr)):
+                tmp = abs(arr[i] - arr[j])
+                if tmp < min_abs_diff:
+                    min_abs_diff = tmp
+                    min_abs_diff_list = []
+                    if arr[i] < arr[j]:
+                        min_abs_diff_list.append([arr[i], arr[j]])
+                    else:
+                        min_abs_diff_list.append([arr[j], arr[i]])
+                elif tmp == min_abs_diff:
+                    if arr[i] < arr[j]:
+                        min_abs_diff_list.append([arr[i], arr[j]])
+                    else:
+                        min_abs_diff_list.append([arr[j], arr[i]])
+
+        min_abs_diff_list.sort()
+        return min_abs_diff_list
+
+# solution 2
 class Solution:
     def minimumAbsDifference(self, arr: List[int]) -> List[List[int]]:
         min_abs_diff = inf
