209dabffccf3928f6ddb4a96a2ac9225d6683d38
Eric <eric900101228@gmail.com>
2026-03-20 00:29:41 +0800

    UVA 10055

---
+++ UVA/10055_Hashmat_the_brave_warrior/solution.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+
+int main() {
+    long long a, b;
+    while (std::cin >> a >> b) {
+        std::cout << std::abs(a - b) << std::endl;
+    }
+
+    return 0;
+}
---
+++ UVA/10055_Hashmat_the_brave_warrior/solution.py
@@ -0,0 +1,7 @@
+while (1):
+    try:
+        a, b = map(int, input().split())
+    except:
+        break
+
+    print(abs(a-b))
